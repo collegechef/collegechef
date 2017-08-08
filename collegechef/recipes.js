108a7bf773148d624bcb7274241cbd0999b82b1d
Kimi Locke <klcheck08@gmail.com>
2017-08-08 15:13:03 -0700

    kimi's additions to the recipies

--- collegechef/recipes.js
+++ collegechef/recipes.js
@@ -5,7 +5,7 @@ var recipes = [
 	"time": "10 minutes",
 	"servings": "2",
 	"ingredients": ["1/2 cup mayo", "1 Tbsp lemon juice", "1/4 tsp ground black pepper", "2 cups chopped, cooked chicken", "1/2 cup blanched silvered almonds", "1 stalk celery, chopped"],
-	"steps": ["1. Combine the dressing incredients", "2. Mix the chicken and veggies", "3. Combine everything"]
+	"steps": ["1. Combine the dressing ingredients", "2. Mix the chicken and veggies", "3. Combine everything"]
 },
 {
 	"name": "Vegan Meatball Subs",
@@ -14,5 +14,31 @@ var recipes = [
 	"servings": "2",
 	"ingredients": [],
 	"steps": []
+},
+{
+  "name": "Avocado Cream Pasta",
+  "type": "Dinner",
+  "time": "15 minutes",
+  "cost": "$5.60",
+  "servings": "2-3",
+  "ingredients": ["2 avocados, pitted and diced", "1 clove garlic, minced", "Juice of 1/2 lemon", "1/4 cup unsweetened soy milk", "1/4 cup water", "Salt, to taste", "Red pepper flakes, to taste", "4 halved cherry tomatoes as garnish (optional)", "2 cups cooked pasta"],
+  "steps": ["1. Combine the avocados, garlic, and lemon juice in a food processor. As the mixture is blending, add the soy milk and then the water. Add more or less water depending on your desired consistency.", "2. Add salt and red pepper, to taste.", "3. Toss with your favorite cooked pasta and serve."],
+},
+{
+  "name": "Sticky Rice",
+  "type": "Dinner",
+  "cost": "$4.17",
+  "servings": "6",
+  "ingredients": ["2 cups of uncooked rice", "Canned or frozen vegetables", "Soy sauce, to taste"],
+  "steps": ["1. Steam your rice.", "2. Microwave your vegetables.", "3. Mix the vegetables, rice, and soy sauce together."],
+},
+{
+  "name": "Black Beans and Rice",
+  "type": "Dinner",
+  "time": "30 minutes",
+  "cost": "$4.79",
+  "servings": "6",
+  "ingredients": ["1 Tbsp. olive oil", "1 large onion, chopped", "2 cloves garlic, minced", "3/4 cup uncooked rice", "1-1/2 cups vegetable broth", "1 tsp. ground cumin", "1/4 tsp. cayenne pepper", "3-1/2 cups canned black beans, drained"],
+  "steps": ["1. Heat your oil over medium heat.", "2. Add the onion and garlic and saute for 4 minutes.", "3. Add the rice and saute for another 2 minutes.", "4. Add in the vegetable broth, bring to a boil, lower the heat and cook for 20 minutes.", "5. Add the spices and black beans immediately before you serve."],
 }
 ];
