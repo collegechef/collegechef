9f79e9987ecb6bc4ca7f7eddeda20afc8173e658
Kimi Locke <klcheck08@gmail.com>
2017-08-08 15:33:03 -0700

    kimi's commit added recipies to js

--- collegechef/recipes.js
+++ collegechef/recipes.js
@@ -51,7 +51,7 @@ var recipes = [
   "servings": "6",
   "ingredients": ["1 Tbsp. olive oil", "1 large onion, chopped", "2 cloves garlic, minced", "3/4 cup uncooked rice", "1-1/2 cups vegetable broth", "1 tsp. ground cumin", "1/4 tsp. cayenne pepper", "3-1/2 cups canned black beans, drained"],
   "steps": ["1. Heat your oil over medium heat.", "2. Add the onion and garlic and saute for 4 minutes.", "3. Add the rice and saute for another 2 minutes.", "4. Add in the vegetable broth, bring to a boil, lower the heat and cook for 20 minutes.", "5. Add the spices and black beans immediately before you serve."],
-}
+},
 {
 	"name": "Mug Macaroni and Cheese",
 	"type": "Lunch",
@@ -59,7 +59,7 @@ var recipes = [
 	"servings": "1",
 	"ingredients": ["1/3 cup pasta", "3/4 cup cold water", "4 tablespoons milk", "1/4 teaspoons cornstarch", "4 tablespoons grated cheddar cheese", "salt", "pepper"],
 	"steps": ["1. In a large microwavable mug or large bowl add in the macaroni and the water. You need a large mug as the water will boil up.", "2. Microwave for roughly 3 ½ minutes. You want the pasta to be fully cooked.", "3. Pour off the remaining cooking water.", "4. Stir in the milk, cornstarch and shredded cheese and microwave for a final 60 seconds to create your sauce. Stir well, season with salt and pepper and enjoy."],
-}
+},
 {
 	"name": "Mug Egg Fried Rice (Gluten-Free)",
 	"type": "Lunch",
@@ -67,8 +67,21 @@ var recipes = [
 	"servings": "1",
 	"ingredients": ["2 cups cooked rice", "1 cup mixed vegetables", "2 large eggs", "2 tablespoons low-sodium tamari", "1 teaspoon sesame seed oil", "1 teaspoon granulated garlic", "1/2 teaspoon five-spice powder", "pinch of salt", "1 stalk of green onions", "sesame seeds for garnish"],
 	"steps": ["1. Place a cup of rice in a large mug. Top with vegetables, enough to fill the mug to the top. Cover with microwave-safe plastic wrap. Puncture a few holes in the plastic wrap with a chopstick or a fork.", "2. Turn the power setting of your microwave to 8/10 and set timer to 1 minute.", "3. While rice is cooking, whisk 1 egg, 1 tablespoon tamari, 1/2 teaspoon sesame oil (if using), 1/2 teaspoon granulated garlic, 1/4 teaspoon five-spice powder, and a small pinch of salt together.", "4. Take the mug out of the microwave, remove plastic wrap, and pour in egg mixture. Use a fork or spoon to give everything a few stirs. Cover rice with plastic wrap again, and microwave rice at an 8/10 power setting for 1 minute 15 to 1 minute 30 seconds.", "5. Using a fork or a spoon, fluff up the rice by stirring the contents several times.", "6. Repeat steps 1 to 4 for the remaining ingredients. Enjoy!"],
-}
+},
 {
-	
-}
+	"name": "Vegan Yogurt Parfait",
+  "type": "Breakfast",
+  "time": "15 minutes",
+  "servings": "2",
+  "ingredients": ["1 6- to 8-ounce container of vanilla, lemon, or piña colada nondairy yogurt (coconut or other)", "1 cup fresh fruit", "Grated semi-sweet chocolate, toasted sliced almonds, or granola for topping"],
+  "steps": ["1. Use a medium size tumbler or parfait cups and for each serving, layer 1/4 cup each of yogurt and fruit in the parfait dish.", "2. Repeat each layer, then do the same for the other parfaits.", "3. Sprinkle with the topping of choice, and serve."],
+},
+{
+  "name": "Classic BLT Sandwich",
+  "type": "Lunch",
+  "time": "5 minutes",
+  "servings": "1",
+  "ingredients": ["2 slices white sandwich bread, toasted", "1 teaspoon unsalted, room temperature butter", "1 to 2 romain or iceberg lettuce leaves", "1 vine ripened tomato, sliced 1/4 inch thick", "3 to 4 basil leaves", "2 slices thickly cut bacon, cooked", "1/2 cup Homemade Mayonnaise", "Coarse salt and freshly ground pepper"],
+  "steps": ["1. Spread one side of one slice of toast with butter.", "2. Arrange lettuce on top, along with tomato and basil. Season with salt and pepper.", "3. Top with bacon.", "4. Spread remaining slice of toast with mayonnaise; place mayonnaise side down to form a sandwich and cut on the diagonal. Serve immediately."],
+},
 ];
