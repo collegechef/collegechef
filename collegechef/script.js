7b09d998e99d35164d586ad52c240dd5ba692238
Kimi Locke <klcheck08@gmail.com>
2017-08-09 10:48:10 -0700

    search bar works, and if you search for correct name it'll output name

--- collegechef/script.js
+++ collegechef/script.js
@@ -1,9 +1,20 @@
 var heading = document.getElementById("testtitle");
 heading.innerHTML = recipes[0].name;
 
-function search(input){
+/***function search(input){
   var input, filter, ul, li, a, i;
   input = document.getElementById("input");
   filter = input.value.toUpperCase();
-  ul
+  for (i = 0; i < recipes.length; i++) {
+    if()
+  }
 }
+***/
+function search(input) {
+  var input = document.getElementById("input");
+  for (var i=0; i < recipes.length; i++) {
+    if (input.value === recipes[i].name) {
+      document.getElementById("testtitle").innerHTML = recipes[i].name;
+    }
+  }
+};
