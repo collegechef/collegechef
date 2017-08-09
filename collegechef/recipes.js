087076a903b0bc10438776a6ccd77848ec7241a7
Swetha <swetha.daz@gmail.com>
2017-08-09 10:50:28 -0700

    Recipe changes

--- collegechef/recipes.js
+++ collegechef/recipes.js
@@ -3,6 +3,7 @@ var recipes = [
 	"name": "Chicken Salad",
 	"type": "Lunch",
 	"time": "10 minutes",
+	"cost": "",
 	"servings": "2",
 	"ingredients": ["1/2 cup mayo", "1 Tbsp lemon juice", "1/4 tsp ground black pepper", "2 cups chopped, cooked chicken", "1/2 cup blanched silvered almonds", "1 stalk celery, chopped"],
 	"steps": ["1. Prep all of the salad ingredients and toss them together in a large bowl.", "2. Prepare the chicken salad dressing in a separate smaller bowl.", "3. Mix together the mayonnaise, preserves, and lemon juice.", "4. Taste for the proper balance of sweetness and acidity. The salad dressing should not be too sweet nor too sour. Add more preserves or lemon juice until you have reached the balance you want. Add salt and pepper to taste.", "5. Gently stir the dressing in with the chicken salad ingredients. Taste and add more salt and pepper if needed."]
@@ -10,6 +11,7 @@ var recipes = [
 {
 	"name": "Vegan Meatball Subs",
 	"type": "Dinner",
+	"time": "",
 	"cost": "$6",
 	"servings": "2",
 	"ingredients": ["8 vegan meatballs", "1/4 cup marinara sauce", "2 hoagie rolls, sliced in half lengthwise", "1/4 cup grated vegan mozzarella"],
@@ -19,7 +21,8 @@ var recipes = [
 "name": "Vegetarian Mug Spinach Ricotta Lasagna",
 "type": "Lunch",
 "time": "10 minutes",
-"servings": "1",
+"cost": "",
+"servings": 1
 "ingredients": ["1/2 fresh lasagna sheet", "2 1/2 cups roughly chopped baby spinach", "1/4 diced medium yellow bell pepper", "1/4 cup part-skim ricotta cheese","3 large finely chopped basil leaves", "1/4 teaspoon kosher salt", "1/8 teaspoon granulated garlic", "6 tablespoons tomato or pasta sauce", "1/3 cup shredded part-skim mozzarella"]
 "steps": ["1. Cut the lasagna sheet in half, and cut each strip in half again. Place the pasta sheets into a bowl, and pour very hot water in it, until it covers all the pieces.", "2. Chop the spinach and place it in a microwave-safe bowl. Cover the top with plastic wrap and poke a few holes. Microwave for 1 minute and let it rest.", "3. Grate the mozzarella while you wait for the spinach to cool.", "4. Mix the spinach with the ricotta, pepper, granulated garlic, and salt. Set mixture aside.", "5. Pour 2 tablespoons of pasta sauce at the bottom of the mug. Top with a piece of softened pasta sheet. Add some spinach mixture, 2 tablespoons of mozzarella, and a pasta sheet. Continue building the lasagna, ending with a layer of pasta sheet at the top. Sprinkle mozzarella over the pasta.", "6. Microwave everything for 1 minute 30 seconds. Check to see that the mozzarella is melted. If it isn't, comtinue microwaving at 15-second intervals.", "7. Serve immediately."],
 },
@@ -107,4 +110,11 @@ var recipes = [
   "ingredients": ["2 slices white sandwich bread, toasted", "1 teaspoon unsalted, room temperature butter", "1 to 2 romain or iceberg lettuce leaves", "1 vine ripened tomato, sliced 1/4 inch thick", "3 to 4 basil leaves", "2 slices thickly cut bacon, cooked", "1/2 cup Homemade Mayonnaise", "Coarse salt and freshly ground pepper"],
   "steps": ["1. Spread one side of one slice of toast with butter.", "2. Arrange lettuce on top, along with tomato and basil. Season with salt and pepper.", "3. Top with bacon.", "4. Spread remaining slice of toast with mayonnaise; place mayonnaise side down to form a sandwich and cut on the diagonal. Serve immediately."],
 },
+{
+	"name": "Savory Microwave Breakfast Mug",
+	"type": "Breakfast",
+	"time": "5 minutes",
+	"cost":"",
+	"ingredients": ["1 cup stale bread pieces*","1/4 cup pre-cooked chopped meat", "1/4 cup finely chopped vegetables", "2 Tbsp shredded cheese", "1 Tbsp butter", "3 Tbsp milk", "1 large egg", "Pinch of salt and pepper"],
+}
 ];
