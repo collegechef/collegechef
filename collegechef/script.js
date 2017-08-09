8837bd51efc2e8cc8a792d2a920de25b29f9cf5e
Kimi Locke <klcheck08@gmail.com>
2017-08-09 09:49:42 -0700

    search bar appears, doesn't work

--- collegechef/script.js
+++ collegechef/script.js
@@ -1,2 +1,9 @@
 var heading = document.getElementById("testtitle");
 heading.innerHTML = recipes[0].name;
+
+function search(input){
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("input");
+  filter = input.value.toUpperCase();
+  ul
+}
