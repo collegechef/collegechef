3566bfa81898b4a5c7457500e901006a6f8a8396
Kimi Locke <klcheck08@gmail.com>
2017-08-09 14:43:52 -0700

    kimi's additions that we're gonna delete

--- collegechef/script.js
+++ collegechef/script.js
@@ -1,20 +1,52 @@
+
 var heading = document.getElementById("testtitle");
 heading.innerHTML = recipes[0].name;
 
-/***function search(input){
-  var input, filter, ul, li, a, i;
-  input = document.getElementById("input");
-  filter = input.value.toUpperCase();
-  for (i = 0; i < recipes.length; i++) {
-    if()
+function gid(a_id) {
+  return document.getElementById(a_id);
+}
+
+function close_all() {
+  for (i=0; i<999; i++){
+    var o = gid(recipes[i].name);
+    if (o) {
+      o.style.display = "none";
+    }
   }
 }
-***/
+
+function find_my_div() {
+  close_all();
+  var input = document.getElementById("input");
+  var o_edit = gid("input");
+  var str_needle = str_needle.toUpperCase();
+
+  if (str_needle != "") {
+    for (i=0; i<999; i++) {
+    var o = gid(recipes[i].name);
+    if (o) {
+      var str_haystack = o.innerHTML.toUpperCase();
+      if (str_haystack.indexOf(str_needle) ==-1) {
+
+      }
+      else {
+        o.style.display = "block";
+      }
+    }
+    }
+  }
+}
+
 function search(input) {
+  var results = [];
   var input = document.getElementById("input");
   for (var i=0; i < recipes.length; i++) {
-    if (input.value === recipes[i].name) {
+    var check = recipes[i].name;
+    var includes = check.includes(input.value);
+    if (includes = true) {
+
       document.getElementById("testtitle").innerHTML = recipes[i].name;
+    includes = false;
     }
   }
 };
