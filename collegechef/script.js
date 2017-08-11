84293ccaef9a9b57ff9ae1ededb11e8909c40035
Angelina Kuo <angelina.kuo@gmail.com>
2017-08-11 10:34:41 -0700

    Angelina adding new carousel and new recipes

--- collegechef/script.js
+++ collegechef/script.js
@@ -1,25 +1,3 @@
-
-var heading = document.getElementById("testtitle");
-heading.innerHTML = recipes[0].name;
-
-function gid(a_id) {};
-
-/**function search(input) {
-  var results = [];
-  var input = document.getElementById("input");
-  for (var i=0; i < recipes.length; i++) {
-    var keyword = "kimi";
-    for (keyword in recipes[i].keywords){
-      if (input.value === recipes[i].keywords[keyword]){
-        results.push(" " + recipes[i].name);
-        results.push(" time: " + recipes[i].time);
-        results.push(" cost: " + recipes[i].cost);
-      }
-    }
-  }
-  document.getElementById("testtitle").innerHTML = results;
-};**/
-
 function search(input) {
   var x="";
   var input = document.getElementById("input");
@@ -33,9 +11,3 @@ function search(input) {
   }
   document.getElementById("testtitle").innerHTML = x;
 };
-
-
-
-//if (input.value === recipes[i].name) {
-  //document.getElementById("testtitle").innerHTML = recipes[i].name;
-//}
